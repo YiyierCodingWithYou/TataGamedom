3f8dbb6da0b776417e745619402b1222b02c58f3
Yiyier <iamkellyoung@gmail.com>
2023-07-05 18:25:40 +0800

    實作優惠券新增、編輯功能

--- TataGamedom/Models/Services/CouponService.cs
+++ TataGamedom/Models/Services/CouponService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.ModelBinding;
+using TataGamedom.Models.Infra;
 using TataGamedom.Models.Interfaces;
 using TataGamedom.Models.ViewModels.Coupons;
 
@@ -18,6 +20,37 @@ namespace TataGamedom.Models.Services
         public IEnumerable<CouponIndexVM> Get()
         {
             return _repo.Get();
+        }
+
+        public Result Create(CouponCreateVM vm)
+        {
+			var selectedStartDate = vm.StartTime.Date;
+			var selectedEndDate = vm.EndTime.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+			bool status =false;
+            if(DateTime.Now>= selectedStartDate && DateTime.Now<= selectedEndDate)
+            {
+                status = true;
+            }
+
+            var create = new CouponCreateVM
+            {
+                Name = vm.Name,
+                Discount = vm.Discount,
+                DiscountTypeId = vm.DiscountTypeId,
+                Description = vm.Description,
+                Threshold = vm.Threshold,
+                StartTime = selectedStartDate,
+                EndTime = selectedEndDate,
+                ActiveFlag = status,
+                CreatedBackendMemberId = vm.CreatedBackendMemberId,
+                CreatedTime = DateTime.Now,
+            };
+            var createResult = _repo.Create(create);
+            if (!createResult)
+            {
+                return Result.Fail("優惠券新增失敗");
+            }
+            return Result.Success();
         }
     }
 }
---
+++ TataGamedom/Models/ViewModels/Coupons/CouponCreateVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TataGamedom.Models.EFModels;
+
+namespace TataGamedom.Models.ViewModels.Coupons
+{
+	public class CouponCreateVM
+	{
+		public int Id { get; set; }
+		[Display(Name = "活動名稱")]
+		[Required]
+		[StringLength(20)]
+		public string Name { get; set; }
+		[Display(Name = "折扣（填入數字）")]
+		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "數字必須大於0")]
+		public int Discount { get; set; }
+		public List<DiscountTypeCode> DiscountTypeCode { get;set;}
+		[Display(Name = "折扣類型")]
+		[Required]
+		public int DiscountTypeId { get; set; }
+		[Display(Name = "活動內容")]
+		[Required]
+		[StringLength(30)]
+		public string Description { get; set; }
+
+		public DateTime CreatedTime { get; set; }
+
+		public int CreatedBackendMemberId { get; set; }
+		[Display(Name = "門檻")]
+		[Required]
+		public int Threshold { get; set; }
+		[Display(Name = "開始時間")]
+		[Required]
+		public DateTime StartTime { get; set; }
+		[Display(Name = "結束時間")]
+		[Required]
+		[EndTimeMustBeAfterStartTime(ErrorMessage = "結束時間不能早於開始時間")]
+		public DateTime EndTime { get; set; }
+		[Display(Name = "活動上線")]
+		[Required]
+		public bool ActiveFlag { get; set; }
+
+		public class EndTimeMustBeAfterStartTimeAttribute : ValidationAttribute
+		{
+			protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+			{
+				var model = (CouponCreateVM)validationContext.ObjectInstance;
+
+				if (model.EndTime < model.StartTime)
+				{
+					return new ValidationResult(ErrorMessage);
+				}
+
+				return ValidationResult.Success;
+			}
+		}
+	}
+}
