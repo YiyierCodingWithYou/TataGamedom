20731de4e5c30bb79819d50d8a723b5fbd95ab18
apalan60 <apalan60@gmail.com>
2023-07-05 18:30:23 +0800

    [v] 隱藏退貨、退款選項, 寄送方式下拉式debug [v] 檢查會員編號是否存在 [v] 訂單完成日期>抵達日期>寄送日期>訂單日期 (完成一半) View => 訂單主檔的部分跟訂單明細的部分拆開，商品圖套用 ; 回首頁 隱藏退貨、退款選項, 寄送方式下拉式debug 畫面呈現正在編輯哪筆訂單 [v] D :  新增讓空的主檔可以被刪除、被關聯的主檔被刪除會被提醒
    [v] 寄信功能，問是否需要寄信;
    [v] 新增自訂驗證

---
+++ TataGamedom/Models/Infra/OrderEmailHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Security.Policy;
+using System.Web.Helpers;
+
+namespace TataGamedom.Models.Infra
+{
+	public class OrderEmailHelper : EmailHelper
+	{
+		private string senderEmail = "apalan60@gmail.com";  /*"TataGamedom @gmail.com"*///;
+		public void SendEmail(string trackingNum, string name, string email)
+		{
+			var subject = "[訂購商品已出貨]";
+			var body = $@"Hi {name}, 您訂購的商品已出貨，貨態追蹤代碼為 {trackingNum} ,再麻煩您留意取貨訊息";
+
+			var from = senderEmail;
+			var to = /*email*/"TataGamedom @gmail.com";
+
+			SendFromGmail(from, to, subject, body);
+		}
+		public virtual void SendFromGmail(string from, string to, string subject, string body)
+		{
+			// todo 以下是開發時,測試之用, 只是建立text file, 不真的寄出信
+			//var path = HttpContext.Current.Server.MapPath("~/files/Mails");
+			//CreateTextFile(path, from, to, subject, body);
+			//return;
+
+			// 以下是實作程式, 可以視需要真的寄出信, 或者只是單純建立text file,供開發時使用
+			// ref https://dotblogs.com.tw/chichiblog/2018/04/20/122816
+			var smtpAccount = from;
+
+			// TODO 請在這裡填入密碼,或從web.config裡讀取
+			var smtpPassword = "pjchjpfhtpfihggj";
+
+			var smtpServer = "smtp.gmail.com";
+			var SmtpPort = 587;
+
+			var mms = new MailMessage();
+			mms.From = new MailAddress(smtpAccount);
+			mms.Subject = subject;
+			mms.Body = body;
+			mms.IsBodyHtml = true;
+			mms.SubjectEncoding = Encoding.UTF8;
+			mms.To.Add(new MailAddress(to));
+
+			using (var client = new SmtpClient(smtpServer, SmtpPort))
+			{
+				client.EnableSsl = true;
+				client.Credentials = new NetworkCredential(smtpAccount, smtpPassword);//寄信帳密 
+				client.Send(mms); //寄出信件
+			}
+		}
+
+//		private void CreateTextFile(string path, string from, string to, string subject, string body)
+//		{
+//			var fileName = $"{to.Replace("@", "_")} {DateTime.Now.ToString("yyyyMMdd_HHmmss")}.txt";
+//			var fullPath = Path.Combine(path, fileName);
+
+//			var contents = $@"from:{from}
+//to:{to}
+//subject:{subject}
+
+//{body}";
+//			File.WriteAllText(fullPath, contents, Encoding.UTF8);
+//		}
+	}
+}
