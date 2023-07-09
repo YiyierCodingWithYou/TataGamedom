063ee098fddd2c99dc8470bf4f730e30e9e58ad2
Jiaen Lee <nn750137@gmail.com>
2023-07-09 16:21:20 +0800

    Complete Moderator

--- TataGamedom/Controllers/Api/BoardsModeratorsApiController.cs
+++ TataGamedom/Controllers/Api/BoardsModeratorsApiController.cs
@@ -14,36 +14,36 @@ using TataGamedom.Models.ViewModels.BoardsModerators;
 
 namespace TataGamedom.Controllers.Api
 {
-    public class BoardsModeratorsApiController : ApiController
-    {
-        private AppDbContext db = new AppDbContext();
+	public class BoardsModeratorsApiController : ApiController
+	{
+		private AppDbContext db = new AppDbContext();
 		private string _connStr = System.Configuration.ConfigurationManager.ConnectionStrings["AppDbContext"].ToString(); //forDapper
 		private SimpleHelper simpleHelper = new SimpleHelper();
 
 		// GET: api/BoardsModeratorsApi
 		public IEnumerable<BoardsModeratorListVm> GetBoardsModerators()
-        {
-            if (db.BoardsModerators==null)
-            {
-                return null;
-            }
-            return db.BoardsModerators.Select(m => new BoardsModeratorListVm
-            {
-                Id = m.Id,
-                BoardName = m.Board.Name,
-                MemberAccount = m.Member.Account,
-                LastOnlineTime = m.Member.LastOnlineTime??DateTime.MinValue,
-                StartDate = m.StartDate,
-                EndDate = m.EndDate,
-                Status = m.EndDate < DateTime.Now ? "退任" : "在任"
-            });
+		{
+			if (db.BoardsModerators == null)
+			{
+				return null;
+			}
+			return db.BoardsModerators.Select(m => new BoardsModeratorListVm
+			{
+				Id = m.Id,
+				BoardName = m.Board.Name,
+				MemberAccount = m.Member.Account,
+				LastOnlineTime = m.Member.LastOnlineTime ?? DateTime.MinValue,
+				StartDate = m.StartDate,
+				EndDate = m.EndDate,
+				Status = m.EndDate < DateTime.Now ? "退任" : "在任"
+			});
 
 		}
 
 		// GET: api/BoardsModeratorsApi/5
 		[ResponseType(typeof(IEnumerable<String>))]
 		public IEnumerable<String> GetBoardsModerator(int boardId)
-        {
+		{
 			IEnumerable<string> boardsModerators = db.BoardsModerators
 				.Where(bm => bm.BoardId == boardId)
 				.Select(bm => bm.Member.Name)
@@ -52,65 +52,65 @@ namespace TataGamedom.Controllers.Api
 			return boardsModerators;
 		}
 
-        // PUT: api/BoardsModeratorsApi/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutBoardsModerator(int id, BoardsModerator boardsModerator)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+		// PUT: api/BoardsModeratorsApi/5
+		[ResponseType(typeof(void))]
+		public IHttpActionResult PutBoardsModerator(int id, BoardsModerator boardsModerator)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
 
-            if (id != boardsModerator.Id)
-            {
-                return BadRequest();
-            }
+			if (id != boardsModerator.Id)
+			{
+				return BadRequest();
+			}
 
-            db.Entry(boardsModerator).State = EntityState.Modified;
+			db.Entry(boardsModerator).State = EntityState.Modified;
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BoardsModeratorExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!BoardsModeratorExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+			return StatusCode(HttpStatusCode.NoContent);
+		}
 
-        // POST: api/BoardsModeratorsApi
-        [ResponseType(typeof(ApiResult))]
-        public ApiResult PostBoardsModerator(BoardsModeratorAddVm vm)
-        {
-            int memberId = simpleHelper.memberIdByAccount(vm.MemberAccount);
+		// POST: api/BoardsModeratorsApi
+		[ResponseType(typeof(ApiResult))]
+		public ApiResult PostBoardsModerator(BoardsModeratorAddVm vm)
+		{
+			int memberId = simpleHelper.memberIdByAccount(vm.MemberAccount);
 			int boardId = simpleHelper.boardIdByName(vm.BoardName);
 
 
-			if (db.BoardsModerators.Any(x => x.ModeratorMemberId == memberId))
-            {
-                return ApiResult.Fail("已經是版主了，不能再加囉!");
-            }
+			if (db.BoardsModerators.Any(x => (x.ModeratorMemberId == memberId && x.EndDate == null)))
+			{
+				return ApiResult.Fail("已經是版主了，不能再加囉!");
+			}
 
-			if (db.BoardsModerators.Count(x => x.BoardId == boardId ) >= 3)
+			if (db.BoardsModerators.Count(x => (x.BoardId == boardId && x.EndDate == null)) >= 3)
 			{
 				return ApiResult.Fail("每版版主最多三人，不能再加囉!");
 			}
 
 			BoardsModerator mod = new BoardsModerator
-            {
-                ModeratorMemberId = memberId,
-                BoardId = boardId,
+			{
+				ModeratorMemberId = memberId,
+				BoardId = boardId,
 				StartDate = DateTime.Now,
-                EndDate = null
+				EndDate = null
 			};
 
 			try
@@ -125,10 +125,10 @@ namespace TataGamedom.Controllers.Api
 			return ApiResult.Success("新增成功");
 		}
 
-        // DELETE: api/BoardsModeratorsApi/5
-        [ResponseType(typeof(ApiResult))]
-        public ApiResult DeleteBoardsModerator(int id)
-        {
+		// DELETE: api/BoardsModeratorsApi/5
+		[ResponseType(typeof(ApiResult))]
+		public ApiResult DeleteBoardsModerator(int id)
+		{
 			BoardsModerator entity = db.BoardsModerators.Find(id);
 
 			if (entity == null)
@@ -156,18 +156,18 @@ namespace TataGamedom.Controllers.Api
 
 
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        private bool BoardsModeratorExists(int id)
-        {
-            return db.BoardsModerators.Count(e => e.Id == id) > 0;
-        }
-    }
+		private bool BoardsModeratorExists(int id)
+		{
+			return db.BoardsModerators.Count(e => e.Id == id) > 0;
+		}
+	}
 }
