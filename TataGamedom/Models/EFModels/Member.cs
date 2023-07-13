4c756112b36296588b84f85da93b43bc2b6616da
apalan60 <apalan60@gmail.com>
2023-07-14 07:45:10 +0800

    重建EFmodels

--- TataGamedom/Models/EFModels/Member.cs
+++ TataGamedom/Models/EFModels/Member.cs
@@ -27,9 +27,9 @@ namespace TataGamedom.Models.EFModels
             NewsViews = new HashSet<NewsView>();
             Orders = new HashSet<Order>();
             PostComments = new HashSet<PostComment>();
+            PostCommentReports = new HashSet<PostCommentReport>();
             PostComments1 = new HashSet<PostComment>();
             PostCommentUpDownVotes = new HashSet<PostCommentUpDownVote>();
-            PostCommentReports = new HashSet<PostCommentReport>();
             PostReports = new HashSet<PostReport>();
             Posts = new HashSet<Post>();
             Posts1 = new HashSet<Post>();
@@ -71,6 +71,8 @@ namespace TataGamedom.Models.EFModels
         public string ConfirmCode { get; set; }
 
         public bool ActiveFlag { get; set; }
+
+        public DateTime? LastOnlineTime { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<BoardsModerator> BoardsModerators { get; set; }
@@ -121,13 +123,13 @@ namespace TataGamedom.Models.EFModels
         public virtual ICollection<PostComment> PostComments { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PostCommentReport> PostCommentReports { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PostComment> PostComments1 { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PostCommentUpDownVote> PostCommentUpDownVotes { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<PostCommentReport> PostCommentReports { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PostReport> PostReports { get; set; }
