4b9375ec1dbae644ca5e4d8e7759fa67fe9f3c0c
apalan60 <apalan60@gmail.com>
2023-07-24 20:59:50 +0800

    API Controller 改到一半

---
+++ TataGamedom_FrontEnd/Controllers/APIContorllers/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TataGamedom_FrontEnd.Models.EFModels;
+using TataGamedom_FrontEnd.Models.Infra.OrderInfra.Queries;
+
+namespace TataGamedom_FrontEnd.Controllers.APIContorllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public OrderController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<Order>>> Get()
+    {
+        return Ok(await _mediator.Send(new GetOrderListQuery()));
+    }
+
+    [HttpGet("{id : int}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
