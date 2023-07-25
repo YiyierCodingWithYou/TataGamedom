2199c32ad534c86d90e2b0d1f769cae608f0f794
apalan60 <apalan60@gmail.com>
2023-07-25 17:32:14 +0800

    .

--- TataGamedomWebAPI/Infrastructure/Data/AppDbContext.cs
+++ TataGamedomWebAPI/Infrastructure/Data/AppDbContext.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace TataGamedomWebAPI.Infrastructure.Data;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options)
+    {
+    }
 }
