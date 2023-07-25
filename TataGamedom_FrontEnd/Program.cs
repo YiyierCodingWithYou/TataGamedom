7f1e3f64f8422d8508a43b8e059b8abf8d328cb7
apalan60 <apalan60@gmail.com>
2023-07-25 08:57:26 +0800

    add log

--- TataGamedom_FrontEnd/Program.cs
+++ TataGamedom_FrontEnd/Program.cs
@@ -15,7 +15,8 @@ namespace TataGamedom_FrontEnd
 
 			// Add services to the container.
 			builder.Services.AddDbContext<AppDbContext>(options =>
-			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+			.LogTo(Console.WriteLine, LogLevel.Information));
 
 			builder.Services.AddControllersWithViews();
 
