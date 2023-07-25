04b60db7485ed6fc7ac083bb03da3fe766b596fc
apalan60 <apalan60@gmail.com>
2023-07-25 17:54:47 +0800

    add Order Infra Controller

---
+++ TataGamedomWebAPI/Infrastructure/OrderInfrastructure/Handlers/UpdateOrderHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+
+using TataGamedom_FrontEnd.Models.Interfaces;
+using TataGamedomWebAPI.Infrastructure.OrderInfrastructure.Commands;
+using TataGamedomWebAPI.Models.EFModels;
+
+namespace TataGamedom_FrontEnd.Models.Infra.OrderInfra.Handlers;
+
+public class UpdateOrderHandler : IRequestHandler<UpdateOrderCommand, int>
+{
+    private readonly IOrderRepository _orderRepository;
+
+    public UpdateOrderHandler(IOrderRepository orderRepository )
+    {
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<int> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+    {
+        Order? order = await _orderRepository.GetOrderByIdAsync(request.Id);
+        if (order == null)
+        {
+            return 0;
+        }
+        else 
+        {
+            order.OrderStatusId = request.OrderStatusId;
+            order.ShipmentStatusId = request.ShipmentStatusId;
+            order.PaymentStatusId = request.PaymentStatusId;
+            //order.CompletedAt = request.CompletedAt;
+            order.ShipmemtMethodId = request.ShipmemtMethodId;
+            //order.RecipientName = request.RecipientName;
+            //order.ToAddress = request.ToAddress;
+            //order.SentAt = request.SentAt;
+            //order.DeliveredAt = request.DeliveredAt;
+            order.TrackingNum = request.TrackingNum;
+
+            return await _orderRepository.UpdateOrderAsync(order);
+        }
+    }
+}
---
+++ TataGamedomWebAPI/Infrastructure/OrderInfrastructure/Queries/GetOrderListQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TataGamedomWebAPI.Models.EFModels;
+
+namespace TataGamedom_FrontEnd.Models.Infra.OrderInfra.Queries;
+
+public record GetOrderListQuery() : IRequest<IEnumerable<Order>>;
