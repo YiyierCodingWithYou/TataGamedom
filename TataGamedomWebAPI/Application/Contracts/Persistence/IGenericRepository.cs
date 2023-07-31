e6d7f59e2d96626d092176f4cfed9f76f29d54e5
apalan60 <apalan60@gmail.com>
2023-07-31 21:00:06 +0800

    Add Application Layer =>IGenericRepository、ApplicationRegistration

---
+++ TataGamedomWebAPI/Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace TataGamedomWebAPI.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IReadOnlyList<T>> GetAsync();
+
+    Task<T> GetByIdAsync(int id);
+
+    Task CreateAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task DeleteAsync(T entity);
+
+}
+
