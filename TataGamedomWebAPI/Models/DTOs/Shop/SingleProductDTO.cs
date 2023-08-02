bc66221027ce758b5c71a79dab87698bce1fc9ec
Yiyier <iamkellyoung@gmail.com>
2023-08-02 17:46:56 +0800

    GetProductsIndex API GetSingleProduct API

---
+++ TataGamedomWebAPI/Models/DTOs/Shop/SingleProductDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace TataGamedomWebAPI.Models.DTOs.Shop
+{
+	public class SingleProductDTO
+	{
+		public int Id { get; set; }
+
+		public string Index { get; set; } = null!;
+
+		public bool IsVirtual { get; set; }
+
+		public int Price { get; set; }
+		public int SpecialPrice { get; set; }
+		public string GamePlatformName { get; set; }
+		public double Score { get; set; }
+
+		public DateTime SaleDate { get; set; }
+
+		public string ChiName { get; set; } = null!;
+
+		public string EngName { get; set; } = null!;
+        public string Description { get; set; }
+
+        public string SystemRequire { get; set; }
+        public bool IsRestrict { get; set; }
+
+		public string GameCoverImg { get; set; } = null!;
+        public IEnumerable<string>? Coupons { get; set; }
+        public IEnumerable<string>? CouponDescription { get; set; }
+        public IEnumerable<string>? ProductImg { get; set; }
+
+        public IEnumerable<string>? Classification { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
