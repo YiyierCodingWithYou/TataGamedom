53f5b537051ab30286d5989ce67226eb1ad4af72
Yiyier <iamkellyoung@gmail.com>
2023-08-04 14:18:04 +0800

    重建Vue專案

---
+++ TataGamedomWebAPI/Models/DTOs/Cart/CartItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using TataGamedomWebAPI.Models.DTOs.Shop;
+
+namespace TataGamedomWebAPI.Models.DTOs.Cart
+{
+	public class CartItemDTO
+	{
+
+		public ProductsDTO? Product { get; set; }
+
+		public int Qty { get; set; }
+		public int SubTotal => Product.SpecialPrice * Qty;
+	}
+}
