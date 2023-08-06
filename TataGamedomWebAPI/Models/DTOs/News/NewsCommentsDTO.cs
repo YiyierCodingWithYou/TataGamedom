f599358d468b09f566fc57861711c0c1df68d69b
KuaN KuaN <zzzz86115@gmail.com>
2023-08-07 05:24:00 +0800

    Kuan0807OK

---
+++ TataGamedomWebAPI/Models/DTOs/News/NewsCommentsDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace TataGamedomWebAPI.Models.DTOs.News
+{
+    public class NewslikesDTO
+	{
+		public int Id { get; set; }
+
+		public int NewsId { get; set; }
+
+		public int MemberId { get; set; }
+
+		public DateTime Time { get; set; }
+	}
+}
