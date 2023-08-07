8109e5b25e5e6716b08fdcba646eef3ecf112270
fuen28 <zzzz86115@gmail.com>
2023-08-07 17:39:56 +0800

    0807

---
+++ TataGamedomWebVuetify/src/views/Members.vue
@@ -0,0 +1,12 @@
+<template>
+<Login/>
+<Register/>
+</template>
+    
+<script setup lang='ts'>
+import Login from '@/components/Members/Login.vue'
+import Register from '@/components/Members/Register.vue'</script>
+    
+<style>
+    
+</style>
