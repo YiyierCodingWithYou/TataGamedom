f1afbc2a5a59d66063e9151a1def4968d440c8a1
KuaN KuaN <zzzz86115@gmail.com>
2023-08-08 01:27:23 +0800

    登入可以用但COOKIE怪怪的

--- TataGamedomWebVuetify/src/views/Members.vue
+++ TataGamedomWebVuetify/src/views/Members.vue
@@ -1,12 +1,14 @@
 <template>
-<Login/>
-<Register/>
+    <RouterLink to="/Members/Login">登入</RouterLink>
+    <br>
+    <RouterLink to="/Members/Register">註冊</RouterLink>
+    <!-- <Login />
+    <Register /> -->
 </template>
     
 <script setup lang='ts'>
-import Login from '@/components/Members/Login.vue'
-import Register from '@/components/Members/Register.vue'</script>
+import LoginVue from '@/components/Members/Login.vue'
+import Register from '@/components/Members/Register.vue'
+</script>
     
-<style>
-    
-</style>
+<style></style>
