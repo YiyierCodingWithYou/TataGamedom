c26c565daaa50361b54bec35c64495c59e0d4e93
fuen28 <zzzz86115@gmail.com>
2023-08-10 18:03:25 +0800

    註冊頁ok

--- TataGamedomWebVuetify/src/components/News/NewsCarousel.vue
+++ TataGamedomWebVuetify/src/components/News/NewsCarousel.vue
@@ -1,40 +1,57 @@
 <template>
-  <v-carousel hide-delimiters style="height: 500px">
-    <v-carousel-item v-for="item in news" :key="item.id" :src="img + item.coverImg" cover>
+  <v-carousel
+    hide-delimiters
+    show-arrows="hover"
+    cycle
+    interval="3000"
+    v-model="currentIndex"
+    style="height: 500px"
+  >
+    <v-carousel-item
+      v-for="item in news"
+      :key="item.id"
+      :src="img + item.coverImg"
+      cover
+    >
       <div class="title">
         <p class="titleword">{{ item.title }}</p>
       </div>
     </v-carousel-item>
   </v-carousel>
+
+  <!-- <div>
+    <v-carousel
+      show-arrows="hover"
+      cycle
+      interval="3000"
+      v-model="currentIndex"
+    >
+      <v-carousel-item
+        v-for="item in news"
+        :src="img + item.coverImg"
+        :key="item.id"
+        cover
+      ></v-carousel-item>
+    </v-carousel>
+  </div> -->
 </template>
     
 
-<script>
-import { ref, onMounted } from "vue";
+<script setup>
+import { ref, reactive, onMounted } from "vue";
 
-export default {
-  setup() {
-    const news = ref([]);
-    const img = "https://localhost:7081/Files/NewsImages/";
-
-    const loadNews = async () => {
-      try {
-        const response = await fetch("https://localhost:7081/api/News");
-        const data = await response.json();
-        news.value = data.news;
-      } catch (error) {
-        console.error("Error loading news:", error);
-      }
-    };
-
-    loadNews();
-
-    return {
-      news,
-      img,
-    };
-  },
+const news = ref([]);
+const img = "https://localhost:7081/Files/NewsImages/";
+const currentIndex = ref(0);
+const loadNews = async () => {
+  const response = await fetch("https://localhost:7081/api/News/HotNews");
+  const data = await response.json();
+  news.value = data;
 };
+
+onMounted(() => {
+  loadNews();
+});
 </script>
     
 
@@ -48,5 +65,10 @@ export default {
 .titleword {
   color: white;
   display: flex;
+  font-size: 60px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 100%;
 }
 </style>
