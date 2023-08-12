c141d6ae1e7f156cd432f23c873dd32d0cfd0f50
apalan60 <apalan60@gmail.com>
2023-08-12 15:00:44 +0800

    調整退貨單建立驗證

--- TataGamedomWebAPI/Application/Features/OrderItemReturn/Commands/CreateOrderItemReturn/CreateOrderItemReturnCommandValidator.cs
+++ TataGamedomWebAPI/Application/Features/OrderItemReturn/Commands/CreateOrderItemReturn/CreateOrderItemReturnCommandValidator.cs
@@ -6,16 +6,31 @@ namespace TataGamedomWebAPI.Application.Features.OrderItemReturn.Commands.Create
 public class CreateOrderItemReturnCommandValidator : AbstractValidator<CreateOrderItemReturnCommand>
 {
     private readonly IOrderItemRepository _orderItemRepository;
+    private readonly IOrderItemReturnRepository _orderItemReturnRepository;
 
-    public CreateOrderItemReturnCommandValidator(IOrderItemRepository orderItemRepository)
+    public CreateOrderItemReturnCommandValidator(
+        IOrderItemRepository orderItemRepository,
+        IOrderItemReturnRepository orderItemReturnRepository)
     {
         this._orderItemRepository = orderItemRepository;
+        this._orderItemReturnRepository = orderItemReturnRepository;
+
 
         RuleFor(p => p.OrderItemId)
             .NotEmpty()
             .MustAsync(OrderItemMustExist)
             .WithMessage("此訂單品項不存在");
-        
+
+        RuleFor(p => p.OrderItemId)
+            .NotEmpty()
+            .MustAsync(OrderItemMustUnique)
+            .WithMessage("此品項已退貨");
+
+    }
+
+    private async Task<bool> OrderItemMustUnique(int orderItemId, CancellationToken token)
+    {
+        return await _orderItemReturnRepository.IsReturnOrderExist(orderItemId) == false;
     }
 
     private async Task<bool> OrderItemMustExist(int orderItemId, CancellationToken token)
