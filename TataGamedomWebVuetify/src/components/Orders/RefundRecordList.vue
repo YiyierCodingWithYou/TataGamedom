1d484f14fbc61c75038156608864e29d198d14d8
apalan60 <apalan60@gmail.com>
2023-08-28 09:09:00 +0800

    改退貨清單API傳回值

---
+++ TataGamedomWebVuetify/src/components/Orders/RefundRecordList.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-dialog v-model="dialog" activator="parent" width="auto">
+    <div class="text-subtitle-2 mb-2">Default</div>
+    <v-expansion-panels>
+      <v-expansion-panel
+        v-for="i in 3"
+        :key="i"
+        title="Item"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      ></v-expansion-panel>
+    </v-expansion-panels>
+  </v-dialog>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
