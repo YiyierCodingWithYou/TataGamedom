3685ecf1d914391e22e5dc372ff8115426ea3e21
Yiyier <iamkellyoung@gmail.com>
2023-08-31 18:31:04 +0800

    0831 ok

--- TataGamedomWebVuetify/src/components/Product/ProductDetail.vue
+++ TataGamedomWebVuetify/src/components/Product/ProductDetail.vue
@@ -23,10 +23,12 @@
                   productData.couponDescription[index] }}<br /></p>
               </div>
               <p class="ml-3 mb-3" style="font-size: 20px; color:white"
-                v-if="productData.specialPrice === productData.price">{{ unitExchange(productData.price)
-                }}</p>
-              <p class="ml-3 mb-3" v-else><s style="font-size: 16px; color:grey">{{ unitExchange(productData.price) }}</s><span
-                  style="font-size: 20px; color:white">　{{ unitExchange(productData.specialPrice) }}</span></p>
+                v-if="productData.specialPrice === productData.price">{{
+                  formattedPrice }}</p>
+              <p class="ml-3 mb-3" v-else-if="productData.specialPrice !== productData.price">
+                <s style="font-size: 16px; color:grey">{{ formattedPrice }}</s>
+                <span style="font-size: 20px; color:white">　{{ formattedSpecialPrice }}</span>
+              </p>
               <div class="d-flex align-center mb-3">
                 <v-rating v-model="productData.score" class="ma-2 d-flex me-auto" density="compact" half-increments
                   readonly style="color:#f9ee08" size="small"></v-rating>
@@ -127,7 +129,7 @@
 </template>
     
 <script setup>
-import { ref, onMounted, defineProps, watch, defineEmits, nextTick } from "vue";
+import { ref, onMounted, defineProps, watch, defineEmits, nextTick, computed } from "vue";
 import { useRouter } from "vue-router";
 import { format } from "date-fns";
 import { zhTW } from "date-fns/locale";
@@ -152,6 +154,21 @@ const emit = defineEmits(["paginationInput", "drawerInput"]);
 watch(props, (newProps) => {
   if (newProps.productData.id) {
     fetchQuantityLimit();
+  }
+});
+const formattedPrice = computed(() => {
+  if (props.productData.price !== undefined) {
+    return unitExchange(props.productData.price);
+  } else {
+    return '';
+  }
+});
+
+const formattedSpecialPrice = computed(() => {
+  if (props.productData.specialPrice !== undefined) {
+    return unitExchange(props.productData.specialPrice);
+  } else {
+    return '';
   }
 });
 
@@ -181,15 +198,15 @@ const decreaseQuantity = () => {
 };
 
 const unitExchange = (x) => {
-      return 'NT$ ' + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+  return 'NT$ ' + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 //加入購物車
 const Add2Cart = async (productId) => {
   let totalQuantity = quantity.value;
 
   if (quantity.value > limit.value) {
-    Swal.fire('加入購物車失敗！', '所選數量超過庫存限制' , 'error');
+    Swal.fire('加入購物車失敗！', '所選數量超過庫存限制', 'error');
     return;
   } else {
     const response = await fetch(`https://localhost:7081/api/Carts/GetSingleProductQuantity?productId=${productId}`, {
@@ -202,7 +219,7 @@ const Add2Cart = async (productId) => {
     totalQuantity = quantity.value + result
     console.log(totalQuantity);
     if (totalQuantity > limit.value) {
-      Swal.fire('加入購物車失敗！', '所選數量加上購物車中現有數量超過庫存限制' , 'error');
+      Swal.fire('加入購物車失敗！', '所選數量加上購物車中現有數量超過庫存限制', 'error');
       return;
     }
   }
@@ -224,7 +241,7 @@ const Add2Cart = async (productId) => {
         name: "Login",
       });
     }
-    Swal.fire('成功！', result.message , 'success');
+    Swal.fire('成功！', result.message, 'success');
     emit("drawerInput", result.message);
   } else {
     let localCart = localStorage.getItem("localCart");
@@ -238,7 +255,7 @@ const Add2Cart = async (productId) => {
     if (existingProduct) {
       totalQuantity = quantityNum.value + existingProduct.qty;
       if (totalQuantity > limit.value) {
-        Swal.fire('加入購物車失敗！', '所選數量加上購物車中現有數量超過庫存限制' , 'error');
+        Swal.fire('加入購物車失敗！', '所選數量加上購物車中現有數量超過庫存限制', 'error');
         return;
       }
       existingProduct.qty += quantityNum.value;
@@ -246,7 +263,7 @@ const Add2Cart = async (productId) => {
       localCart.push({ productId, qty: quantityNum.value });
     }
     localStorage.setItem("localCart", JSON.stringify(localCart));
-    Swal.fire('成功！', '商品已加入購物車' , 'success');
+    Swal.fire('成功！', '商品已加入購物車', 'success');
     emit("drawerInput", "已成功加入購物車！");
   }
 };
@@ -316,7 +333,7 @@ const commentSubmit = async () => {
     .then((result) => {
       if (result.message === "發表評論成功") {
         (comment.value = ""), (star.value = 0);
-        alert(result.message);
+        Swal.fire('', result.message, 'success');
         emit("commentSucceed");
         returnComments();
       } else {
@@ -324,7 +341,7 @@ const commentSubmit = async () => {
       }
     })
     .catch((error) => {
-      console.error("Error:", error);
+      Swal.fire('', error.message, 'error');
     });
 };
 
