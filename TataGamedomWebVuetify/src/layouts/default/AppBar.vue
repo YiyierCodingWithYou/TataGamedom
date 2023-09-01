d8e1319a95a14d9db48877c7b10093534ccdad8b
Jiaen Lee <nn750137@gmail.com>
2023-09-01 16:18:34 +0800

    ✨Metge!!!!🦦

--- TataGamedomWebVuetify/src/layouts/default/AppBar.vue
+++ TataGamedomWebVuetify/src/layouts/default/AppBar.vue
@@ -50,7 +50,17 @@
       icon="mdi-cart-outline"
       @click="link('Cart')"
     ></v-btn>
+    <div v-if="$store.state.isLoggedIn" class="mx-2">
+      <img
+        :src="iconImg"
+        @mouseover="showMemberProfile = true"
+        style="margin-top: 0px"
+      />
+    </div>
     <div v-if="$store.state.isLoggedIn" class="h-100 pageBtn">
+      <!-- <a color="primary" @mouseover="showMemberProfile = true">
+        HI {{ name }}
+      </a> -->
       <v-btn
         variant="text"
         rounded="0"
